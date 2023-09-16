fd1a73c69190cc85abd9e300c5d0b01aa27cbabe
Camila Jesus <camiladejesusfontes@gmail.com>
2023-09-16 01:10:18 -0300

    atualizando backend

--- database.py
+++ database.py
@@ -3,12 +3,12 @@ import os
 from flask import g
 
 def connect_to_database():
-    db_path = os.path.join(os.path.dirname(__file__), 'listapendencias.db')
+    db_path = os.path.join(os.path.dirname(__file__), 'apptarefas.db')
     sql = sqlite3.connect(db_path)
     sql.row_factory = sqlite3.Row
     return sql
 
 def get_database():
-    if not hasattr(g, "listapendencias_db"):
-        g.listapendencias_db = connect_to_database()
-    return g.listapendencias_db
+    if not hasattr(g, "apptarefas_db"):
+        g.apptarefas_db = connect_to_database()
+    return g.apptarefas_db
